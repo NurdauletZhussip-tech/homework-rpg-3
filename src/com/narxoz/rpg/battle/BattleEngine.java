bf80b223ba7f887f382c2cac5af043e7fd76f8c0
NurdauletZhussip-tech <nurdaulet.zhussip.studying@gmail.com>
2026-02-28 19:37:48 +0500

    adapter and singletone work

--- src/com/narxoz/rpg/battle/BattleEngine.java
+++ src/com/narxoz/rpg/battle/BattleEngine.java
@@ -1,11 +1,12 @@
 package com.narxoz.rpg.battle;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public final class BattleEngine {
     private static BattleEngine instance;
-    private Random random = new Random(1L);
+    private Random random = new Random();
 
     private BattleEngine() {
     }
@@ -22,17 +23,100 @@ public final class BattleEngine {
         return this;
     }
 
-    public void reset() {
-        // TODO: reset any battle state if you add it
-    }
-
     public EncounterResult runEncounter(List<Combatant> teamA, List<Combatant> teamB) {
-        // TODO: validate inputs and run round-based battle
-        // TODO: use random if you add critical hits or target selection
+        // валидация инпутов и начинаем битву
+        int rounds = 0;
+        double critRoll;
+        // проверка строк
+        if(teamA.isEmpty() || teamB.isEmpty()) {
+            EncounterResult fail = new EncounterResult();
+            fail.setWinner("One of the teams is empty");
+            fail.setRounds(0);
+            fail.addLog("Error in team input");
+            return fail;
+        }
         EncounterResult result = new EncounterResult();
-        result.setWinner("TBD");
-        result.setRounds(0);
-        result.addLog("TODO: implement battle simulation");
+        result.addLog("►Battle started");
+        result.addLog("Team A:");
+        for(Combatant a : teamA) {
+            result.addLog("\t" + a.getName() + " → hp: " + a.getHealth() + "; atk: " + a.getAttackPower() + "; crit%: " + a.getCritChance()*100 + "%");
+        }
+        result.addLog("Team B:");
+        for(Combatant b : teamB) {
+            result.addLog("\t" + b.getName() + " → hp: " + b.getHealth() + "; atk: " + b.getAttackPower());
+        }
+        // процессы раунда
+        while (!teamA.isEmpty() && !teamB.isEmpty()) {
+            rounds++;
+            result.addLog("—".repeat(20) + "\nRound: " + rounds);
+            for(Combatant hero : teamA) {
+                Combatant heroTarget = teamB.get(selectRandom(teamB));
+                critRoll = random.nextDouble(0, 1);
+                int damage = hero.getAttackPower();
+                if(critRoll <= hero.getCritChance()){
+                    damage *= 2;
+                }
+                heroTarget.takeDamage(damage);
+                result.addLog(String.format("\t[\"%s\" took %d damage from \"%s\"]", heroTarget.getName(), damage, hero.getName()));
+                if(!heroTarget.isAlive()){
+                    result.addLog(String.format("[%s fainted...]", heroTarget.getName()));
+                    teamB.remove(heroTarget);
+                }
+
+            }
+            for(Combatant enemy : teamB) {
+                if(!enemy.isAlive()){
+                }
+                else{
+                    Combatant enemyTarget = teamA.get(selectRandom(teamA));
+                    enemyTarget.takeDamage(enemy.getAttackPower());
+                    result.addLog(String.format("\t[\"%s\" took %d damage from \"%s\"]", enemyTarget.getName(), enemy.getAttackPower(), enemy.getName()));
+                    if(!enemyTarget.isAlive()){
+                        result.addLog(String.format("[%s fainted...]", enemyTarget.getName()));
+                        teamA.remove(enemyTarget);
+                    }
+                }
+            }
+
+            result.addLog("Team A:");
+            for(Combatant a : teamA) {
+                result.addLog("\t" + a.getName() + " → hp: " + a.getHealth() + "; atk: " + a.getAttackPower());
+            }
+            result.addLog("Team B:");
+            for(Combatant b : teamB) {
+                result.addLog("\t" + b.getName() + " → hp: " + b.getHealth() + "; atk: " + b.getAttackPower());
+            }
+        }
+        if(teamA.isEmpty()) {
+            result.setWinner("Enemies won...");
+        }
+        else{
+            result.setWinner("Heroes won!");
+        }
+        result.setRounds(rounds);
+        result.addLog("►Battle ended");
         return result;
     }
-}
+
+    private int selectRandom(List<Combatant> opposingTeam) {
+        int fullAggro = 1;
+        ArrayList<Integer> aggroList = new ArrayList<>();
+        int id = 0;
+        for(Combatant character : opposingTeam){
+            fullAggro += character.getAggro();
+            aggroList.add(character.getAggro());
+        }
+        int enemySelects = random.nextInt(1, fullAggro);
+        for(int i = 0; i < aggroList.size(); i++){
+            if(enemySelects >= aggroList.get(i)){
+                enemySelects -= aggroList.get(i);
+            }
+            else{
+                id = i;
+                break;
+            }
+        }
+        return id;
+    }
+    }
+
